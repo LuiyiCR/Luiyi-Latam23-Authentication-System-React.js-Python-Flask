aa2eb50c3ad01f0ce3cdaf7a837d9327aadbccca
Luis Salazar O <luiyi_salazar@hotmail.com>
2024-01-12 17:34:59 +0000

    Set the Salt

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,12 +5,12 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from bcrypt import gensalt
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
-
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -20,3 +20,15 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/user", methods=["POST"])
+def handle_register():
+    # Verify that the request has the required fields
+    # Verify that the user doesn't already exist
+    # Create the salt
+    salt = str(gensalt())
+    # Create the hashed_password password + salt
+    # Create the user
+    # Save the user in the DB
+    # Return 201
+    return jsonify({"salt": salt}), 200
